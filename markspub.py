6f74e1ce23396ffa1f3137a27c5ad1a6498bfb07
Noah Diekemper <32496549+NonContradiction@users.noreply.github.com>
2025-07-19 15:44:50 -0400

    Update markspub.py

--- markspub.py
+++ markspub.py
@@ -64,5 +64,5 @@ if button_clicked:
 
 st.markdown("---")  # Optional horizontal rule
 
-st.markdown("ℹ️ You may need to double-click the button the first time. If you're on a computer, you should be able to use the enter/return key instead of the GUI.")
+st.markdown("ℹ️ You may need to double-click the button the first time. If you're on a computer, you should be able to use the enter/return key instead of the GUI (after clicking on it once).")
 st.markdown("𓂀𓋹𓁈𓃠𓆃☥𓆣")
